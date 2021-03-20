06236b89d8edaa11d4c4d9a032ea4ad8dc7f065c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-03-20 16:18:11 +0300

    V priloženii na "Qt" risujetsä okno s risovanijem sçeny. Poka cto cerez kostyli i s ispoljzovanijem "SDL2".

--- src/QtEditor/main.cpp
+++ src/QtEditor/main.cpp
@@ -1,4 +1,7 @@
-#include <QtGui/QGuiApplication>
+#include  "../SDL2ViewerLib/Host.hpp"
+#include <QtCore/QTimer>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
 
 namespace SZV
 {
@@ -6,9 +9,36 @@ namespace SZV
 namespace
 {
 
+// Workaround for old Qt without QVulkanWindow. Reuse code from SDL2 Viewer to create Vulkan window.
+
+class HostWrapper final : public QWidget
+{
+public:
+
+	HostWrapper()
+	{
+		connect(&timer_, &QTimer::timeout, this, &HostWrapper::Loop);
+		timer_.start(20);
+	}
+
+private:
+	void Loop()
+	{
+		if(host_.Loop())
+			close();
+	}
+
+private:
+	Host host_;
+	QTimer timer_;
+};
+
 int Main(int argc, char* argv[])
 {
-	QGuiApplication app(argc, argv);
+	QApplication app(argc, argv);
+
+	HostWrapper window;
+	window.show();
 
 	return app.exec();
 }
